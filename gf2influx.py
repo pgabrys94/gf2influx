a74ab17264fc718543e496d456dc68b597ee8984
Paweł Gabryś <p.gabrys@onet.eu>
2024-03-28 08:53:47 +0100

    v1.1 added log file existence check

--- gf2influx.py
+++ gf2influx.py
@@ -13,35 +13,36 @@ fields_list = ["sequence_num", "bytes", "packets"]
 try:
     db_client = InfluxDBClient("changeMe", 8086, "changeMe", "changeMe", "changeMe")
     while True:
-        with subprocess.Popen(args, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as f:
-            p = select.poll()
-            p.register(f.stdout)
+        if os.path.exists(temp_file):
+            with subprocess.Popen(args, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as f:
+                p = select.poll()
+                p.register(f.stdout)
 
-            if p.poll():
-                tags = {}
-                fields = {}
+                if p.poll():
+                    tags = {}
+                    fields = {}
 
-                line = json.loads(f.stdout.readline())
+                    line = json.loads(f.stdout.readline())
 
-                flow_time = (float(line["time_flow_end_ns"]) - float(line["time_flow_start_ns"])) / 1e9
-                fields["flow_time"] = flow_time
+                    flow_time = (float(line["time_flow_end_ns"]) - float(line["time_flow_start_ns"])) / 1e9
+                    fields["flow_time"] = flow_time
 
-                for key, value in line.items():
-                    if key in tags_list:
-                        tags[key] = value
-                    elif key in fields_list:
-                        fields[key] = value
+                    for key, value in line.items():
+                        if key in tags_list:
+                            tags[key] = value
+                        elif key in fields_list:
+                            fields[key] = value
 
-                formatted = [
-                    {
-                        "measurement": line["type"],
-                        "tags": tags,
-                        "fields": fields,
-                        "timestamp": line["time_received_ns"]
-                    }
-                ]
+                    formatted = [
+                        {
+                            "measurement": line["type"],
+                            "tags": tags,
+                            "fields": fields,
+                            "timestamp": line["time_received_ns"]
+                        }
+                    ]
 
-                db_client.write_points(formatted)
+                    db_client.write_points(formatted)
 
 except Exception as err:
     print(err)
