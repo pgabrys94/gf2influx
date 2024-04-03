f773da61cbf9e33c339e70c5b0326e8e8e192d00
Paweł Gabryś <p.gabrys@onet.eu>
2024-04-03 07:22:51 +0200

    v1.3 changed single inserts into batch inserts

--- gf2influx.py
+++ gf2influx.py
@@ -16,43 +16,47 @@ def send_to_influxdb(data):
 
 
 def poller():
-    batch = []
-    counter = 0
-    while True:
-        if os.path.exists(temp_file):
-            with subprocess.Popen(args, stdout=subprocess.PIPE) as f:
-                p = select.poll()
-                p.register(f.stdout)
+    try:
+        batch = []
+        counter = 0
+        while True:
+            if os.path.exists(temp_file):
+                with subprocess.Popen(args, stdout=subprocess.PIPE) as f:
+                    p = select.poll()
+                    p.register(f.stdout)
 
-                if p.poll():
-                    tags = {}
-                    fields = {}
+                    if p.poll():
+                        tags = {}
+                        fields = {}
 
-                    line = json.loads(f.stdout.readline().decode())
+                        line = json.loads(f.stdout.readline().decode())
 
-                    flow_time = (float(line["time_flow_end_ns"]) - float(line["time_flow_start_ns"])) / 1e9
-                    fields["flow_time"] = flow_time
+                        flow_time = (float(line["time_flow_end_ns"]) - float(line["time_flow_start_ns"])) / 1e9
+                        fields["flow_time"] = flow_time
 
-                    for key, value in line.items():
-                        if key in tags_list:
-                            tags[key] = value
-                        elif key in fields_list:
-                            fields[key] = value
+                        for key, value in line.items():
+                            if key in tags_list:
+                                tags[key] = value
+                            elif key in fields_list:
+                                fields[key] = value
 
-                    formatted = {
-                        "measurement": line["type"],
-                        "tags": tags,
-                        "fields": fields,
-                        "timestamp": line["time_received_ns"]
-                    }
+                        formatted = {
+                            "measurement": line["type"],
+                            "tags": tags,
+                            "fields": fields,
+                            "timestamp": line["time_received_ns"]
+                        }
 
-                    batch.append(formatted)
-                    counter += 1
+                        batch.append(formatted)
+                        counter += 1
 
-            if counter == 50:
-                threading.Thread(target=send_to_influxdb, args=(batch.copy(),)).start()
-                batch.clear()
-                counter = 0
+                if counter == 50:
+                    threading.Thread(target=send_to_influxdb, args=(batch.copy(),)).start()
+                    batch.clear()
+                    counter = 0
+    except Exception as error:
+        print("ERROR: ", error)
+        print("LINE: ", line)
 
 
 temp_file = os.path.normpath("/var/log/netflow.log")
