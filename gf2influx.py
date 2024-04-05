a86b35bf6d97c770e5cb28358df4721e6d134fbe
Paweł Gabryś <p.gabrys@onet.eu>
2024-04-05 13:35:13 +0200

    v1.6 splitted script to threaded functions for better performance and reliability

--- gf2influx.py
+++ gf2influx.py
@@ -5,7 +5,6 @@ import json
 from conson import Conson
 import sys
 import threading
-import time
 from datetime import datetime, timedelta
 from influxdb import InfluxDBClient
 
@@ -17,16 +16,64 @@ def send_to_influxdb(data):
         print("InfluxDB write error: ", str(type(error)) + ": " + str(error))
 
 
+def digester(data):
+    tags_list = ["proto", "in_if", "out_if", "sampler_address", "src_addr", "dst_addr", "src_port", "dst_port"]
+    fields_list = ["sequence_num", "bytes", "packets"]
+    tags = {}
+    fields = {}
+    batch = set()
+
+    i = len(data)
+    for raw_line in data:
+        line = ""
+        try:
+            if raw_line.startswith(b"{") and raw_line.endswith(b"}\n"):
+                line = json.loads(raw_line)
+            else:
+                print("SKIPPED: ", raw_line)
+                continue
+        except Exception as error:
+            column = 0
+            words = str(error).split()
+            if "column" in words:
+                column = int(words[words.index("column") + 1])
+            print(datetime.now().isoformat(), str(error) + ":", str(line)[:column] +
+                  ("<-" if column != 0 else ""))
+            print("LINE\n", line, "\n")
+            print("RAW_LINE\n", raw_line, "\n")
+            continue
+
+        flow_time = (float(line["time_flow_end_ns"]) - float(line["time_flow_start_ns"])) / 1e9
+        fields["flow_time"] = flow_time
+
+        for key, value in line.items():
+            if key in tags_list:
+                tags[key] = str(value)
+            elif key in fields_list:
+                fields[key] = value
+
+        formatted = {
+            "measurement": line["type"],
+            "tags": tags,
+            "time": int(line["time_received_ns"]),
+            "fields": fields
+        }
+
+        batch.add(formatted)
+        i -= 1
+
+    if i == 0:
+        threading.Thread(target=send_to_influxdb, args=(batch.copy(),)).start()
+        batch.clear()
+
+
+
 temp_file = os.path.normpath("/var/log/netflow.log")
 args = ['tail', '-fn0', temp_file]
 
-tags_list = ["proto", "in_if", "out_if", "sampler_address", "src_addr", "dst_addr", "src_port", "dst_port"]
-fields_list = ["sequence_num", "bytes", "packets"]
 config = Conson(salt="geoip2grafana")
 config_file = os.path.join(os.getcwd(), "config.json")
 pwd = ""
-previous_time = datetime.now()
-batch = []
 
 try:
     if os.path.exists(config_file):
@@ -57,65 +104,22 @@ except Exception as err:
 
 try:
     db_client = InfluxDBClient(config()["host"], config()["port"], config()["username"], pwd, config()["database"])
-    while True:
-        try:
+    lines = set()
+    previous_time = datetime.now()
+    with subprocess.Popen(args, stdout=subprocess.PIPE) as f:
+        p = select.poll()
+        p.register(f.stdout)
+        while True:
             if os.path.exists(temp_file):
-                with subprocess.Popen(args, stdout=subprocess.PIPE) as f:
-                    p = select.poll()
-                    p.register(f.stdout)
 
-                    if p.poll():
-                        tags = {}
-                        fields = {}
-                        line = ""
+                if p.poll():
+                    lines.add(f.stdout.readline())
 
-                        raw_line = f.stdout.readline()
-
-                        try:
-                            if raw_line.startswith(b"{") and raw_line.endswith(b"}\n"):
-                                line = json.loads(raw_line)
-                            else:
-                                print("SKIPPED: ", raw_line)
-                                continue
-                        except Exception as error:
-                            column = 0
-                            words = str(error).split()
-                            if "column" in words:
-                                column = int(words[words.index("column") + 1])
-                            print(datetime.now().isoformat(), str(error) + ":", str(line)[:column] +
-                                  ("<-" if column != 0 else ""))
-                            print("LINE\n", line, "\n")
-                            print("RAW_LINE\n", raw_line, "\n")
-                            continue
-
-                        flow_time = (float(line["time_flow_end_ns"]) - float(line["time_flow_start_ns"])) / 1e9
-                        fields["flow_time"] = flow_time
-
-                        for key, value in line.items():
-                            if key in tags_list:
-                                tags[key] = str(value)
-                            elif key in fields_list:
-                                fields[key] = value
-
-                        formatted = {
-                            "measurement": line["type"],
-                            "tags": tags,
-                            "time": int(line["time_received_ns"]),
-                            "fields": fields
-                        }
-
-                        batch.append(formatted)
-                        time.sleep(0.01)
-
-
-                now = datetime.now()
-                if now - previous_time > timedelta(seconds=5):
-                    threading.Thread(target=send_to_influxdb, args=(batch.copy(),)).start()
-                    batch.clear()
-                    previous_time = now
-
-        except Exception as error:
-            print("ERROR: ", error)
+                    now = datetime.now()
+                    if datetime.now() - previous_time > timedelta(seconds=5) and len(lines) != 0:
+                        threading.Thread(target=digester, args=(lines.copy(),)).start()
+                        lines.clear()
+                        previous_time = now
 
 except Exception as err:
     print(str(type(err)) + ": " + str(err))
